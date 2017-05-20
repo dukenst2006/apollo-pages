5f0afc000035af9e99340a5759a643faa80e27b9
Diego Lorenzo <diego@diegolorenzo.com>
2017-05-20 03:24:27 -0400

    Trying out potential testing solution.

--- tests/PagesTest.php
+++ tests/PagesTest.php
@@ -1,10 +1,23 @@
 <?php
 
+use Weerd\ApolloPages\Models\ApolloPage;
+
 class PagesTest extends TestCase
 {
     /** @test */
     public function it_can_do_some_action()
     {
         // Test code here.
+
+        $page = ApolloPage::create([
+            'slug' => 'something',
+            'path' => '/something',
+            'parent_id' => null,
+            'tier' => 1,
+            'title' => 'Something Title',
+            'body' => 'stuff',
+        ]);
+
+        dump(ApolloPage::all());
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,39 +1,45 @@
 <?php
 
 use Illuminate\Database\Capsule\Manager as DB;
+use Orchestra\Testbench\TestCase as BaseTestCase;
 
-abstract class TestCase extends PHPUnit_Framework_TestCase
+abstract class TestCase extends BaseTestCase
 {
+    protected function getEnvironmentSetUp($app)
+    {
+        $this->setUpDatabase($app);
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            \Weerd\ApolloPages\ApolloPagesServiceProvider::class
+        ];
+    }
+
     /**
      * Setup the test case data persistance layer.
      */
     public function setUp()
     {
-        $this->setUpDatabase();
-        $this->migrateTables();
+        parent::setUp();
+
+        $this->artisan('migrate', ['--database' => 'apollo_pages']);
+
+        // Cannot run scaffold command since not expected directory structure...
+        // $this->artisan('apollo:scaffold');
     }
 
     /**
      * Setup the in-memory database.
      */
-    protected function setUpDatabase()
+    protected function setUpDatabase($app)
     {
-        $database = new DB;
-
-        $database->addConnection(['driver' => 'sqlite', 'database' => ':memory:']);
-        $database->bootEloquent();
-        $database->setAsGlobal();
-    }
-
-    /**
-     * Run any migrations to setup the schema for the database.
-     */
-    public function migrateTables()
-    {
-        DB::schema()->create('pages', function ($table) {
-            $table->increments('id');
-            $table->string('title');
-            $table->timestamps();
-        });
+        $app['config']->set('database.default', 'apollo_pages');
+        $app['config']->set('database.connections.apollo_pages', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
     }
 }
